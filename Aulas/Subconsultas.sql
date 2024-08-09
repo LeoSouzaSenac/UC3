afb7ad33ab4a9d22709f992d5fe8543b237bc2d9
LeoSouzaSenac <152904287+LeoSouzaSenac@users.noreply.github.com>
2024-08-09 20:01:32 -0300

    Update Subconsultas.sql

--- Aulas/Subconsultas.sql
+++ Aulas/Subconsultas.sql
@@ -254,10 +254,45 @@ A consulta retornará os nomes dos departamentos onde a média salarial dos carg
 
 1) Escreva uma consulta SQL para selecionar o departamento_id e a soma dos salários para cada departamento. Filtre os departamentos onde a soma total dos salários é maior que R$ 12.000,00.
 
+RESPOSTA:
+SELECT departamento_id, SUM(salario) AS total_salario
+FROM cargos
+GROUP BY departamento_id
+HAVING SUM(salario) > 12000.00;
+
+
+
 2) Escreva uma consulta SQL para selecionar o titulo e o número de empregados para cada título. Filtre os títulos onde há mais de 2 empregados.
+
+RESPOSTA:
+SELECT titulo, COUNT(empregados_id) AS numero_empregados
+FROM empregados
+GROUP BY titulo
+HAVING COUNT(empregados_id) > 2;
+
 
 3) Escreva uma consulta SQL para selecionar o departamento_id e a média dos salários para cada departamento. Filtre os departamentos onde a média salarial é maior que R$ 4.500,00.
 
+RESPOSTA:
+SELECT departamento_id, AVG(salario) AS media_salario
+FROM cargos
+GROUP BY departamento_id
+HAVING AVG(salario) > 4500.00;
+
+
 4) Escreva uma consulta SQL para selecionar o departamento_id e o maior salário para cada departamento. Filtre os departamentos onde o maior salário é superior a R$ 6.000,00.
 
+RESPOSTA:
+SELECT departamento_id, MAX(salario) AS maior_salario
+FROM cargos
+GROUP BY departamento_id
+HAVING MAX(salario) > 6000.00;
+
+
 5) Escreva uma consulta SQL para selecionar o departamento_id e o menor salário para cada departamento. Filtre os departamentos onde o menor salário é superior a R$ 2.500,00.
+
+RESPOSTA:
+SELECT departamento_id, MIN(salario) AS menor_salario
+FROM cargos
+GROUP BY departamento_id
+HAVING MIN(salario) > 2500.00;
